a96c75865e71d013a219083545b64cdcab690204
rahulramaprasad7 <rara3459@colorado.edu>
2019-10-18 11:19:54 -0600

    All conditions double checked

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #define HEAP_SIZE 4000
-__xdata char __sdcc_heap[HEAP_SIZE];         
+__xdata char __sdcc_heap[HEAP_SIZE];
 const unsigned int __sdcc_heap_size = HEAP_SIZE;   //Set the Heap size to 4000 bytes
 
 uint16_t lastCount = 0;
@@ -29,9 +29,9 @@ void printBuffers (struct buff** head_ref)
     int k = 0, j = 0;
     while(temp!= NULL)
     {
-        printf_tiny("\n\r****Information regarding buffer_%d****\n\r", k);
-        printf("\n\rStarting address of buffer_%d = %p\n\r", k, &temp->characters[0]);
-        printf("Ending address of buffer_%d = %p\n\r", k, &(temp->characters[temp->size]));
+        printf_tiny("\n\r****Information regarding buffer_%d****\n\r", temp->bufferNo);
+        printf("\n\rStarting address of buffer_%d = %p\n\r", temp->bufferNo, &temp->characters[0]);
+        printf("Ending address of buffer_%d = %p\n\r", temp->bufferNo, &(temp->characters[temp->size]));
         printf_tiny("The size of this buffer = %d\n\r", temp->size);
         if( k == 0)
         {
@@ -48,8 +48,8 @@ void questionMark()
 {
     uint16_t i, j = 0;
     printBuffers(&buffer_0);
-    printf_tiny("Number of total characters since last ? = %d\n\r", ((buffer_0->characterCount) - lastCount));
-    printf_tiny("Contents of Buffer_0 are \n\r");
+    printf_tiny("\n\rNumber of total characters since last ? = %d\n\r", ((buffer_0->characterCount) - lastCount));
+    printf_tiny("\n\rContents of Buffer_0 are \n\r");
     for (i = 0; i < buffer_0->characterCount; i += 64)
     {
         for (j = i; j < (i+64) && j < buffer_0->characterCount; j++)
@@ -142,33 +142,37 @@ void deleteNode(struct buff **head_ref)
     uint16_t position = atoi(x);
 
     if (*head_ref == NULL)
+    {
+        printf_tiny("There are no buffers to be deleted\n\r");
         return;
+    }
 
     struct buff* temp = *head_ref;
+    struct buff* prev = NULL;
 
     if (position == 0)
     {
         printf_tiny("Buffer_0 cannot be deleted using this command, please type the next character\n\r");
         return;
     }
-    for (i = 1; temp != NULL && i < position; i++)
+    while (temp != NULL && temp->bufferNo != position)
+    {
+        prev = temp;
         temp = temp->next;
+    }
 
-    if (temp == NULL || temp->next == NULL)
+    if (temp == NULL)
+    {
+        printf_tiny("Please enter a valid buffer number to delete \n\r");
         return;
+    }
+    printf_tiny("\n\rSuccessfully deleted buffer_%d\n\r", temp->bufferNo);
+    prev->next = temp->next;
 
-    struct buff *next = temp->next->next;
+    free(temp);
 
-    free(temp->next->characters);
-    free(temp->next);
-
-    printf_tiny("\n\r Successfully deleted buffer_%d\n\r", temp->next->bufferNo);
-
-    temp->next->characters = NULL;
-    temp->next = NULL;
-
-    temp->next = next;
-    temp->next->bufferNo = next->bufferNo;
+    temp->characters = NULL;
+    temp = NULL;
 }
 
 void deleteAll(struct buff **head_ref)
@@ -194,7 +198,7 @@ void reallocate()
     while(1)
     {
         buffer_0 = malloc(sizeof(struct buff));          //Allocate memory for meta-data for buffer_0
-        printf_tiny("Enter a buffer size\n\r");          
+        printf_tiny("Enter a buffer size\n\r");
         gets(x);										 //Input the size of buffer
         buffer_0->size = atoi(x);						 //Convert string to int
         if (!(buffer_0->size >= 32 && buffer_0->size <=3200 && (buffer_0->size % 16 == 0)))      //Check for buffer size conditions imposed by the question
@@ -216,7 +220,7 @@ void reallocate()
         {
             free(buffer_1->characters);										//Free the buffer_1
             free(buffer_1);													//Free Buffer_1 meta-data
-            buffer_1->characters = NULL;									//Set the buffer pointer to NULL 
+            buffer_1->characters = NULL;									//Set the buffer pointer to NULL
             &buffer_1 == NULL;
             free(buffer_0->characters);										//Free the buffer_0
 			free(buffer_0);                                                 //Free buffer_0 meta-data
@@ -227,6 +231,7 @@ void reallocate()
         }
         break;																//Break out of the loop if buffer_0 and buffer_1 are successfully allocated
     }
+    printf_tiny("Successfully allocated the buffers\n\r");
     buffer_0->next = buffer_1;												//Point buffer_0 to next node buffer_1
     buffer_1->next = NULL;													//Point the next node of buffer_1 to NULL for time being
     buffer_0->bufferNo = 0;													//Assign buffer number
@@ -244,18 +249,14 @@ void main()
 {
     uint16_t ch, i = 0, j = 0;
     reallocate();								//Allocate buffer_0 and buffer_1 successfully
-    if (buffer_1->characters == NULL)			
-    {
-        reallocate();
-    }
 
     buffer_0->characterCount = 0;				//Initialise character count of buffer_0
     printf_tiny("Enter the characters\n\r");    //Start inputting characters serially
     do
     {
         ch = getchar();                         								//Store each character
-        (buffer_0->characterCount)++;           							    //Incremenet character count after each input 
-        buffer_0->freeCharacters = buffer_0->size - buffer_0->characterCount;   //Calculate free space
+        (buffer_0->characterCount)++;           							    //Incremenet character count after each input
+        buffer_0->freeCharacters = buffer_0->size - i;                          //Calculate free space
         if (ch >= 97 && ch <= 122)												//Store the lowercase characters in buffer_0
         {
             if (i >= buffer_0->size)											//If buffer_0 limit is reached exho the characters
@@ -293,7 +294,7 @@ void main()
             putchar(ch);
             printf_tiny("\n\r");
             deleteAll(&buffer_0);
-            reallocate();										                //Start executing program again 
+            reallocate();										                //Start executing program again
             printf_tiny("Enter the characters\n\r");
             continue;
         }
@@ -308,9 +309,7 @@ void main()
             putchar(ch);
             continue;
         }
-
-    }while (ch != '\r');                                                        //Continue reading serial inputs until "Enter" key is pressed
-    return;
+    }while (1);                                                        //Continue reading serial inputs until "Enter" key is pressed
 }
 int putchar (int c)
 {
@@ -327,21 +326,5 @@ int getchar ()
 	return SBUF;  	// return character from SBUF
 }
 
-/*int* mgets()
-{
-	int *ptr;
-	int *x = ptr;
-	int rd, i = 0;
-    do
-    {
-        rd = getchar();
-        ptr[i] = rd;
-        printf("%d ", ptr[i++]);
-    }while (rd != '\r');
-
-	*ptr = '\0';
-	return x;
-}
-*/
 
 
